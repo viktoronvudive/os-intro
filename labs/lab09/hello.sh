09eff360ecf6aad0d19a7723aa366722792c5599
viktoronvudive <1032215834@pfur.ru>
2023-04-04 06:06:41 -0700

    lab 9

---
+++ labs/lab09/hello.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+HELLO=Hello
+function hello {
+local HELLO=Worldlocal HELLO=Worldlocal HELLO=World
+echo $HELLO
+}
+echo $HELLO
+hello
+function hello {
+local HELLO=Worldlocal HELLO=Worldlocal HELLO=World
+echo $HELLO
+}
+echo $HELLO
+hello
+function hello {
+local HELLO=Worldlocal HELLO=Worldlocal HELLO=World
+echo $HELLO
+}
+echo $HELLO
+hello
+function hello {
+local HELLO=Worldlocal HELLO=Worldlocal HELLO=World
+echo $HELLO
+}
+echo $HELLO
+hello
